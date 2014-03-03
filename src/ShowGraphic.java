8a769307ca2e135bf7fb2d3d901b81c0e0eef7e8
Tyler Bell <tbupper90@swbell.net>
2014-03-03 12:05:59 -0600

    Added Graphics class

---
+++ src/ShowGraphic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ShowGraphic 
+{
+	public void makeBarGraph(LinkedHashMap<String, Region> list, String sortMethod)
+	{
+		
+	}
+	
+	public void makeWorldMap(LinkedHashMap<String, City> cities)
+	{
+		
+	}
+}
