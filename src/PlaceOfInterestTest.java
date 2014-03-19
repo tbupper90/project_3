bbfdc9e0308d7d34f43de8003773a4b3e4533ff4
avkrakkle <thayer.terry.a@gmail.com>
2014-03-19 09:11:01 -0500

    Fixed PlaceOfInterestTest
    The test now creates an arraylist of fake countries to pass to the
    PlaceOfInterest object's constructor, and then asserts that the
    arraylist returned by PlaceOfInterest.getCountry is equal to the fake
    countries.

--- src/PlaceOfInterestTest.java
+++ src/PlaceOfInterestTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
@@ -8,13 +10,17 @@ public class PlaceOfInterestTest {
     @Test
     public void testPlaceOfInterest() {
         // Test correct storage of attributes
+        ArrayList<Country> testCountries = new ArrayList<>();
+        testCountries.add(new Country("Name1", "Pop1", "Area1", "Continent1"));
+        testCountries.add(new Country("Name2", "Pop2", "Area2", "Continent2"));
+
         PlaceOfInterest testPlace = new PlaceOfInterest("Name", "Area",
-                "Description", "Country");
+                "Description", testCountries);
 
         assertEquals(testPlace.getName(), "Name");
         assertEquals(testPlace.getArea(), "Area");
         assertEquals(testPlace.getDescription(), "Description");
-        assertEquals(testPlace.getCountry(), "Country");
+        assertEquals(testPlace.getCountry(), testCountries);
     }
 
 }
