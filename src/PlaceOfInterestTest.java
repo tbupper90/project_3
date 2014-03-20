d1fa5e65ba4cfe2f9b1df51237a5c693e88f8e23
Tyler Bell <tbupper90@swbell.net>
2014-03-19 23:02:22 -0500

    GUI Updated
    I think we are ready to produce the graphical output!

--- src/PlaceOfInterestTest.java
+++ src/PlaceOfInterestTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //import static org.junit.Assert.*;
 //
 //import org.junit.Test;
@@ -19,31 +18,3 @@
 //    }
 //
 //}
-=======
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-
-import org.junit.Test;
-
-
-public class PlaceOfInterestTest {
-
-    @Test
-    public void testPlaceOfInterest() {
-        // Test correct storage of attributes
-        ArrayList<Country> testCountries = new ArrayList<>();
-        testCountries.add(new Country("Name1", "Pop1", "Area1", "Continent1"));
-        testCountries.add(new Country("Name2", "Pop2", "Area2", "Continent2"));
-
-        PlaceOfInterest testPlace = new PlaceOfInterest("Name", "Area",
-                "Description", testCountries);
-
-        assertEquals(testPlace.getName(), "Name");
-        assertEquals(testPlace.getArea(), "Area");
-        assertEquals(testPlace.getDescription(), "Description");
-        assertEquals(testPlace.getCountry(), testCountries);
-    }
-
-}
->>>>>>> FETCH_HEAD
